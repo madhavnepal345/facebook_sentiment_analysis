bfc6fc48debf84904681b2e2daa87f57c8ea5c42
madhav <madhavnepal916@gmail.com>
2025-05-21 21:20:33 +0545

    loaded the pretrained model

--- backend/models/train_model.py
+++ backend/models/train_model.py
@@ -28,21 +28,19 @@ class SentimentDataset(Dataset):
             return_tensors='pt'
 
         )
-        return{
-            'input_ids':encoding['input_ids'].flatten(),
-            'attention_mask':encoding['attention_mask'].flatten()
-            'label':torch.tensor(label,dtype=torch.long)
-            }
-
+        return{'input_ids':encoding['input_ids'].flatten(),
+                'attention_mask':encoding['attention_mask'].flatten(),
+                'labels':torch.tensor(label,dtype=torch.long)}
+    
 class BERTModelTrainer:
     def __init__ (self):
         self.device=torch.device("cuda" if torch.cuda.is_available()else"cpu")
         self.tokenizer=BertTokenizer.from_pretrained('bert-base-uncased')
         self.label_map={'negative':0,'neutral':1,'positive':2}
         self.reverse_label_map={v:k for k,v in self.label_map.items()}
-
-
-    def load_data(self.filepath:str):
+    
+    def load_data(self,data_path:str):
+        # loading the data
         df=pd.read_csv(filepath)
         df['cleaned_text']=df['text'].apply(self.preprocess_text)
         df['label']=df['sentiment'].map(self.label_map)
@@ -66,6 +64,34 @@ class BERTModelTrainer:
         train_datasets=SentimentDataset(
             train_df['cleaned_text'].tolist(),
             train_df['label'].tolist(),
-            sdelf.tokenizer,
+            self.tokenizer,
             max_lenght=128
+        )
+
+        eval_dataset=SentimentDataset(
+            eval_df['cleaned_text'].tolist(),
+            eval_df['label'].tolist(),
+            self.tokenizer,
+            max_length=128
+        )
+
+        #loading the models
+
+        model= BertForSequenceClassification.from_pretrained('bert-base-uncased',num_labels=len(self.label_map))
+        model.to(self.device)
+
+
+        #training arguments
+        training_args=TrainingArguments(
+            output_dir='./results',
+            num_train_epochs=3,
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=16,
+            warmup_steps=500,
+            weight_decay=0.01,
+            logging_dir='./logs',
+            logging_steps=10,
+            evaluation_strategy="epoch",
+            save_strategy="epoch",
+            load_best_model_at_end=True
         )

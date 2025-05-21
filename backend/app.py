7769201dfad652d57d7bda990247f2bebd532c62
madhav <madhavnepal916@gmail.com>
2025-05-21 20:59:49 +0545

    some changes in app

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ from utils.auth import(
 app = FastAPI()
 limiter =Limiter(key_func=get_remote_address)
 app.state.limiter=limiter
-app.add_exception_handler(RateLimitedExceeded,_rate_limit_exceeded_handler)
+app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
